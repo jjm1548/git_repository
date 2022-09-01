d6010eb301eecc72531bbfb6d8903df3c6566406
jiangjieming <710804243@qq.com>
2022-09-01 11:28:46 +0800

    change 01

---
+++ src/main/java/test/test.java
@@ -0,0 +1,11 @@
+package test;
+
+public class test {
+    public static void main(String[] args) {
+        int a,b,c;
+        a = 10;
+        b = 20;
+        c = 30;
+        System.out.printf("%d + %d = %d",a,b,c);
+    }
+}
